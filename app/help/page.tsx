07f11a758cb0370e96a6003e019d87badacc18d1
Praveen Juge <hello@praveenjuge.com>
2024-07-18 19:26:35 +0530

    Fixes
    Signed-off-by: Praveen Juge <hello@praveenjuge.com>

--- app/help/page.tsx
+++ app/help/page.tsx
@@ -57,7 +57,7 @@ export default function Page() {
             <CardHeader>
               <CardTitle>{category}</CardTitle>
             </CardHeader>
-            <CardContent className="flex flex-col text-primary">
+            <CardContent className="flex flex-col gap-2 text-primary">
               {posts.map((post) => (
                 <Link key={post.slug} href={`/help/${post.slug}`}>
                   {post.title}
