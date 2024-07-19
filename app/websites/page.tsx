5ac69cea4f948a5e97aaab36312a03a8a92ef564
Praveen Juge <hello@praveenjuge.com>
2024-07-19 22:43:14 +0530

    Websites page empty state
    Signed-off-by: Praveen Juge <hello@praveenjuge.com>

--- app/websites/page.tsx
+++ app/websites/page.tsx
@@ -13,8 +13,8 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { createClient } from "@/lib/supabase/server";
-import { SignedIn } from "@clerk/nextjs";
-import { Globe } from "@mynaui/icons-react";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { Earth, Globe } from "@mynaui/icons-react";
 import { Metadata } from "next";
 import { AddWebsite } from "./AddWebsite";
 import { DeleteWebsite } from "./DeleteWebsite";
@@ -35,13 +35,29 @@ export default async function Page() {
 
   return (
     <>
-      <div className="flex justify-between">
+      <div className="flex items-center justify-between">
         <CardHeader className="p-0">
           <CardTitle>{metadata.title as string}</CardTitle>
           <CardDescription>{metadata.description}</CardDescription>
         </CardHeader>
-        <AddWebsite />
+        <SignedIn>
+          <AddWebsite />
+        </SignedIn>
       </div>
+      <SignedOut>
+        <div className="flex w-full flex-col items-center justify-center rounded border-[0.5px] bg-primary-foreground px-4 py-20 text-center">
+          <div className="mx-auto rounded-full border-[0.5px] bg-background p-2">
+            <Earth className="size-6" />
+          </div>
+          <h3 className="mb-1 mt-2 text-sm font-medium">
+            Add your websites here
+          </h3>
+          <p className="mb-4 text-balance text-sm text-muted-foreground">
+            When you add a website you will get a special URL to get your OG
+            Images for that website.
+          </p>
+        </div>
+      </SignedOut>
       <SignedIn>
         <Card>
           <Table>
@@ -55,38 +71,46 @@ export default async function Page() {
               </TableRow>
             </TableHeader>
             <TableBody>
-              {websites.map((website) => (
-                <TableRow key={website.id}>
-                  <TableCell>
-                    <div className="flex items-center gap-2">
-                      {website.favicon_url ? (
-                        <img src={website.favicon_url} className="size-5" />
-                      ) : (
-                        <Globe className="size-5" />
-                      )}
-                      <span className="max-w-xs truncate font-medium">
-                        {website.title ? website.title : website.website_url}
-                      </span>
-                    </div>
-                  </TableCell>
-                  <TableCell>{website.website_url}</TableCell>
-                  <TableCell>{website.total_count}</TableCell>
-                  <TableCell>
-                    {/* TODO */}
-                    30 Days
-                  </TableCell>
-                  <TableCell>
-                    <div className="flex items-center gap-2">
-                      <EditWebsite
-                        websiteId={website.id}
-                        currentUrl={website.website_url}
-                      />
-                      <span>•</span>
-                      <DeleteWebsite websiteId={website.id} />
-                    </div>
+              {websites.length > 0 ? (
+                websites.map((website) => (
+                  <TableRow key={website.id}>
+                    <TableCell>
+                      <div className="flex items-center gap-2">
+                        {website.favicon_url ? (
+                          <img src={website.favicon_url} className="size-4" />
+                        ) : (
+                          <Globe className="size-4" />
+                        )}
+                        <span className="max-w-xs truncate font-medium">
+                          {website.title ? website.title : website.website_url}
+                        </span>
+                      </div>
+                    </TableCell>
+                    <TableCell>{website.website_url}</TableCell>
+                    <TableCell>{website.total_count}</TableCell>
+                    <TableCell>
+                      {/* TODO */}
+                      30 Days
+                    </TableCell>
+                    <TableCell>
+                      <div className="flex items-center gap-2">
+                        <EditWebsite
+                          websiteId={website.id}
+                          currentUrl={website.website_url}
+                        />
+                        <span>•</span>
+                        <DeleteWebsite websiteId={website.id} />
+                      </div>
+                    </TableCell>
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={5} className="text-center">
+                    No websites found. Add a new website to get started.
                   </TableCell>
                 </TableRow>
-              ))}
+              )}
             </TableBody>
           </Table>
         </Card>
