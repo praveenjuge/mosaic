422654adf09c49507a770355a72f074e0863a4f9
Praveen Juge <hello@praveenjuge.com>
2024-07-24 11:55:36 +0530

    Add a11y
    Signed-off-by: Praveen Juge <hello@praveenjuge.com>

--- components/spinner.tsx
+++ components/spinner.tsx
@@ -13,20 +13,21 @@ export const LoadingSpinner = ({
   return (
     <div className="grid size-full place-items-center">
       <svg
-        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+        fill="none"
         width={size}
         height={size}
-        {...props}
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
         strokeWidth="2"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
+        xmlns="http://www.w3.org/2000/svg"
         className={cn("animate-spin", className)}
       >
         <path d="M21 12a9 9 0 1 1-6.219-8.56" />
       </svg>
+      <span className="sr-only">Loading...</span>
     </div>
   );
 };
