0ac394fee5faee2734346381121543ebb47f8593
Naveen <naveendurai19@gmail.com>
2024-07-31 16:50:25 +0530

    Trying with both bar & line as chart

--- app/analytics/imageschart.tsx
+++ app/analytics/imageschart.tsx
@@ -5,25 +5,26 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
-import { CartesianGrid, XAxis, YAxis, Bar, BarChart, Legend } from "recharts";
+import { CartesianGrid, XAxis, YAxis, Bar, BarChart, Legend, Line, ComposedChart } from "recharts";
 
 export function ImagesChart({ page_hits }: { page_hits: any }) {
   let data: any[] = []
   if (page_hits) {
     data = Object.keys(page_hits).map((date) => ({
-      time: new Date(date).toISOString(),
+      time: date,
       total_hits: page_hits[date],
     }));
   }
   return (
     <ChartContainer className="h-44 w-full pb-10" config={{}}>
-      <BarChart
+      <ComposedChart
         data={data}
         margin={{ left: 12, right: 12, top: 12, bottom: 12 }}
       >
         <CartesianGrid vertical={false} />
         <XAxis
           dataKey="time"
+          hide={true}
           tickFormatter={(value) => new Date(value).toLocaleDateString()}
           // tick={{ angle: -15, textAnchor: 'end', dx: 25 }}
           tickLine={false}
@@ -34,14 +35,15 @@ export function ImagesChart({ page_hits }: { page_hits: any }) {
           tickMargin={2}
           allowDataOverflow={false}
         />
-        <Legend />
+        <Legend wrapperStyle={{ paddingTop: '15px' }} />
 
         <ChartTooltip
           cursor={false}
-          content={<ChartTooltipContent hideLabel />}
+          content={<ChartTooltipContent />}
         />
-        <Bar dataKey="total_hits" fill="#07976a" />
-      </BarChart>
+        {/* <Bar dataKey="total_hits" fill="#07976a" /> */}
+        <Line type="monotone" dataKey="total_hits" stroke="#07976a" />
+      </ComposedChart>
     </ChartContainer>
   );
 }
