a72a3df24143491f81c0ebb18e0fe37a25ab8b8c
Naveen <naveendurai19@gmail.com>
2024-07-31 17:00:30 +0530

    Trying with both bar & line as chart

--- app/analytics/imageschart.tsx
+++ app/analytics/imageschart.tsx
@@ -5,10 +5,45 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
-import { CartesianGrid, XAxis, YAxis, Bar, BarChart, Legend, Line, ComposedChart } from "recharts";
+import { CartesianGrid, XAxis, YAxis, Bar, BarChart, Legend, Line, AreaChart, Area } from "recharts";
+import { curveCardinal } from 'd3-shape';
+
 
 export function ImagesChart({ page_hits }: { page_hits: any }) {
+  page_hits = {
+    "2024-07-02": 10,
+    "2024-07-03": 30,
+    "2024-07-04": 20,
+    "2024-07-05": 10,
+    "2024-07-06": 15,
+    "2024-07-07": 20,
+    "2024-07-08": 15,
+    "2024-07-09": 25,
+    "2024-07-10": 30,
+    "2024-07-11": 25,
+    "2024-07-12": 30,
+    "2024-07-13": 25,
+    "2024-07-14": 20,
+    "2024-07-15": 15,
+    "2024-07-16": 10,
+    "2024-07-17": 85,
+    "2024-07-18": 80,
+    "2024-07-19": 125,
+    "2024-07-20": 100,
+    "2024-07-21": 150,
+    "2024-07-22": 100,
+    "2024-07-23": 125,
+    "2024-07-24": 100,
+    "2024-07-25": 12,
+    "2024-07-26": 6,
+    "2024-07-27": 10,
+    "2024-07-28": 15,
+    "2024-07-29": 10,
+    "2024-07-30": 15,
+    "2024-07-31": 10
+  }
   let data: any[] = []
+  const cardinal = curveCardinal.tension(0.2);
   if (page_hits) {
     data = Object.keys(page_hits).map((date) => ({
       time: date,
@@ -16,10 +51,9 @@ export function ImagesChart({ page_hits }: { page_hits: any }) {
     }));
   }
   return (
-    <ChartContainer className="h-44 w-full pb-10" config={{}}>
-      <ComposedChart
+    <ChartContainer className="h-44 w-full" config={{}}>
+      <AreaChart
         data={data}
-        margin={{ left: 12, right: 12, top: 12, bottom: 12 }}
       >
         <CartesianGrid vertical={false} />
         <XAxis
@@ -39,11 +73,10 @@ export function ImagesChart({ page_hits }: { page_hits: any }) {
 
         <ChartTooltip
           cursor={false}
-          content={<ChartTooltipContent />}
+          content={<ChartTooltipContent hideLabel />}
         />
-        {/* <Bar dataKey="total_hits" fill="#07976a" /> */}
-        <Line type="monotone" dataKey="total_hits" stroke="#07976a" />
-      </ComposedChart>
+        <Area type={cardinal} dataKey="total_hits" stroke="#07976a" fill="#07976a" fillOpacity={0.3} hide={false} />
+      </AreaChart>
     </ChartContainer>
   );
 }
