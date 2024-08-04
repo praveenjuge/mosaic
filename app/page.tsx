7068d5a10056d5c98a1a773b3d0ec49758279861
Praveen Juge <hello@praveenjuge.com>
2024-08-04 20:38:37 +0530

    New Landing Page Designs
    Signed-off-by: Praveen Juge <hello@praveenjuge.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,92 @@
+import CTA from "@/components/home/cta";
+import HomeSignedIn from "@/components/home/homesignedin";
 import Logo from "@/components/logo";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { getOgImageUrl } from "@/lib/utils";
 import {
   ClerkLoading,
-  SignedIn,
   SignedOut,
   SignInButton,
   SignUpButton,
 } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
-import { ArrowRight, CheckHexagon } from "@mynaui/icons-react";
-import WebsitesTable from "./websites/WebsitesTable";
-import FetchWebsitePagesData from "@/components/server/fetch-website-pages-data";
-import LatestScreenshots from "@/components/latest-screenshots";
+import {
+  ArrowLongDown,
+  ArrowLongRight,
+  CheckHexagon,
+} from "@mynaui/icons-react";
+import { Metadata } from "next";
+import Image from "next/image";
+import { Suspense } from "react";
 
+export const metadata: Metadata = {
+  title: "Simplify Your Open Graph Image Creation.",
+  description:
+    "Transform your website's Open Graph (OG) social images by automating the process using screenshots. Say goodbye to the hassle of designing OG images for every page — let your beautiful website do the talking.",
+  openGraph: { images: [getOgImageUrl("")] },
+};
 
-export default async function Home() {
-  let websitePagesData = []
-  const response = await FetchWebsitePagesData({ page: 1, limit: 5 });
-  if (!response || !response?.data) {
-    console.log("No data");
-  } else {
-    websitePagesData = response.data;
-  }
+const features = [
+  "No Code Required",
+  "Fully Automated",
+  "No Credit Card Required",
+];
 
+const landingContent = [
+  {
+    title: "Automated OG Image Creation",
+    points: [
+      "Generate eye-catching social media OG images",
+      "Showcase your beautiful website design effortlessly",
+      "Ensure your social media previews always look perfect",
+    ],
+    image: "/images/landing-1.png",
+  },
+  {
+    title: "Easy Websites Integration",
+    points: [
+      "Add your website to Mosaic in just a few clicks",
+      "No coding skills required",
+      "Quick access to important metrics and recent screenshots",
+    ],
+    image: "/images/landing-2.png",
+  },
+  {
+    title: "Customizable Analytics",
+    points: [
+      "Track your OG image performance with detailed analytics",
+      "Visualize data with interactive charts and graphs",
+      "Handle high-volume requests with ease",
+    ],
+    image: "/images/landing-3.png",
+  },
+];
+
+const FeaturePoint = ({ children }: { children: React.ReactNode }) => (
+  <div className="flex items-center gap-2">
+    <CheckHexagon className="size-5 shrink-0 stroke-2 text-primary" />
+    <span>{children}</span>
+  </div>
+);
+
+export default function Home() {
   return (
     <>
       <SignedOut>
-        <div className="mx-auto flex max-w-xl flex-col items-center gap-7 text-center">
+        <div className="mx-auto flex max-w-xl flex-col items-center gap-7 py-10 text-center">
           <Logo />
-          <div className="space-y-2">
+          <div className="space-y-3">
             <h1 className="text-balance text-3xl font-semibold tracking-tighter">
-              Simplify Your Open Graph Image Creation.
+              {metadata.title as string}
             </h1>
-            <p className="text-pretty text-base text-muted-foreground">
-              Transform your website into a stunning Open Graph image with
-              Mosaic. Simply input your URL, and get a high-quality snapshot
-              ready for social media.
+            <p className="text-balance text-base text-muted-foreground">
+              {metadata.description}
             </p>
           </div>
           <div className="flex gap-2">
             <ClerkLoading>
               <Button size="lg" disabled>
                 Start for Free
-                <ArrowRight className="ml-2 size-4" stroke={2} />
+                <ArrowLongRight className="ml-2 size-4" stroke={2} />
               </Button>
               <Button variant="outline" size="lg" disabled>
                 Sign In
@@ -57,7 +95,7 @@ export default async function Home() {
             <SignUpButton mode="modal">
               <Button size="lg">
                 Start for Free
-                <ArrowRight className="ml-2 size-4" stroke={2} />
+                <ArrowLongRight className="ml-2 size-4" stroke={2} />
               </Button>
             </SignUpButton>
             <SignInButton mode="modal">
@@ -67,57 +105,111 @@ export default async function Home() {
             </SignInButton>
           </div>
           <div className="flex flex-col items-center gap-4 text-muted-foreground md:flex-row">
-            <div className="flex items-center gap-2">
-              <CheckHexagon />
-              <span>No Code Required</span>
-            </div>
-            <div className="flex items-center gap-2">
-              <CheckHexagon />
-              <span>Fully Automated</span>
-            </div>
-            <div className="flex items-center gap-2">
-              <CheckHexagon />
-              <span>No Credit Card Required</span>
-            </div>
+            {features.map((feature, index) => (
+              <div key={index} className="flex items-center gap-2">
+                <CheckHexagon />
+                <span>{feature}</span>
+              </div>
+            ))}
           </div>
         </div>
+        <section className="mx-auto my-6 flex max-w-5xl flex-col gap-20">
+          <div className="flex flex-col gap-10 text-xs md:flex-row">
+            <div className="flex flex-col divide-y-[0.5px] overflow-hidden rounded-lg border-[0.5px]">
+              <Image
+                src={`/images/original-example-og.jpg`}
+                alt={`Original Open Graph Image`}
+                className="h-60 w-full bg-emerald-50 object-cover"
+                width={600}
+                height={250}
+              />
+              <div className="flex flex-col gap-1 p-4">
+                <p className="font-semibold">
+                  To Use or Not to Use Auto Layout in Figma
+                </p>
+                <p>
+                  The debate on whether or not to use Auto Layout in Figma is a
+                  hot topic that frequently surfaces on Twitter. Based on my
+                  experience with this…
+                </p>
+                <p className="text-muted-foreground">praveenjuge.com</p>
+              </div>
+            </div>
+            <div className="grid shrink-0 place-items-center text-muted-foreground">
+              <ArrowLongRight className="hidden size-7 md:block" />
+              <ArrowLongDown className="size-7 md:hidden" />
+            </div>
+            <div className="flex flex-col divide-y-[0.5px] overflow-hidden rounded-lg border-[0.5px]">
+              <Image
+                src={`/images/mosaic-example-og.jpg`}
+                alt={`Mosaic Open Graph Image`}
+                className="h-60 w-full bg-emerald-50 object-cover"
+                width={600}
+                height={250}
+              />
+              <div className="flex flex-col gap-1 p-4">
+                <p className="font-semibold">
+                  To Use or Not to Use Auto Layout in Figma
+                </p>
+                <p>
+                  The debate on whether or not to use Auto Layout in Figma is a
+                  hot topic that frequently surfaces on Twitter. Based on my
+                  experience with this…
+                </p>
+                <p className="text-muted-foreground">praveenjuge.com</p>
+              </div>
+            </div>
+          </div>
+          {landingContent.map((content, index) => (
+            <div
+              key={index}
+              className="grid grid-cols-1 items-center gap-10 md:grid-cols-2"
+            >
+              {index % 2 === 0 ? (
+                <>
+                  <div className="flex flex-col gap-4">
+                    <h2 className="text-xl font-semibold">{content.title}</h2>
+                    <div className="flex flex-col gap-4">
+                      {content.points.map((point, pointIndex) => (
+                        <FeaturePoint key={pointIndex}>{point}</FeaturePoint>
+                      ))}
+                    </div>
+                  </div>
+                  <Image
+                    src={content.image}
+                    alt={content.title}
+                    className="w-full bg-emerald-50 object-cover"
+                    width={500}
+                    height={327}
+                  />
+                </>
+              ) : (
+                <>
+                  <Image
+                    src={content.image}
+                    alt={content.title}
+                    className="w-full bg-emerald-50 object-cover"
+                    width={500}
+                    height={327}
+                  />
+                  <div className="order-first flex flex-col gap-4 md:order-none">
+                    <h2 className="text-xl font-semibold">{content.title}</h2>
+                    <div className="flex flex-col gap-4">
+                      {content.points.map((point, pointIndex) => (
+                        <FeaturePoint key={pointIndex}>{point}</FeaturePoint>
+                      ))}
+                    </div>
+                  </div>
+                </>
+              )}
+            </div>
+          ))}
+        </section>
+        <CTA />
       </SignedOut>
-      <SignedIn>
-        <div>
-          <CardHeader className="mb-4 p-0">
-            <CardTitle>Overview</CardTitle>
-          </CardHeader>
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-            <Card>
-              <CardHeader>
-                <CardTitle>413</CardTitle>
-                <CardDescription>Images</CardDescription>
-              </CardHeader>
-            </Card>
-            <Card>
-              <CardHeader>
-                <CardTitle>30 Days</CardTitle>
-                <CardDescription>Cached</CardDescription>
-              </CardHeader>
-            </Card>
-            <Card>
-              <CardHeader>
-                <CardTitle>Free Plan</CardTitle>
-                <CardDescription>Subscription</CardDescription>
-              </CardHeader>
-            </Card>
-          </div>
-        </div>
-        <div>
-          <CardHeader className="mb-4 p-0">
-            <CardTitle>Websites</CardTitle>
-          </CardHeader>
-          <WebsitesTable />
-        </div>
-        <div>
-          <LatestScreenshots websitePages={websitePagesData} showPagination={false} />
-        </div>
-      </SignedIn>
+      <Suspense>
+        <HomeSignedIn />
+      </Suspense>
     </>
   );
 }
---
+++ components/home/cta.tsx
@@ -0,0 +1,46 @@
+import { ClerkLoaded, ClerkLoading, SignUpButton } from "@clerk/nextjs";
+import { ArrowLongRight } from "@mynaui/icons-react";
+import Image from "next/image";
+import { Button } from "../ui/button";
+
+export default function CTA() {
+  return (
+    <section className="pb-10">
+      <div className="mx-auto grid max-w-5xl grid-cols-1 items-center gap-6 overflow-hidden rounded-lg border-[0.5px] border-black border-opacity-15 bg-gradient-to-r from-lime-100 via-emerald-100 to-teal-100 p-6 md:grid-cols-2 md:gap-12 md:px-10">
+        <div className="space-y-4">
+          <div className="space-y-2">
+            <h2 className="text-xl font-semibold tracking-tight md:text-3xl">
+              Simplify OG Images.
+            </h2>
+            <p className="text-balance text-base opacity-60">
+              Generate click-worthy OG images in minutes, no coding required.
+            </p>
+          </div>
+          <ClerkLoading>
+            <Button variant="outline" disabled>
+              Start for Free
+              <ArrowLongRight className="ml-2 size-4" stroke={2} />
+            </Button>
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignUpButton mode="modal">
+              <Button variant="outline">
+                Start for Free
+                <ArrowLongRight className="ml-2 size-4" stroke={2} />
+              </Button>
+            </SignUpButton>
+          </ClerkLoaded>
+        </div>
+        <div className="relative h-80">
+          <Image
+            alt="CTA Image"
+            width={600}
+            height={400}
+            src="/images/analytics.png"
+            className="absolute left-0 top-0 h-96 max-w-none rounded-lg bg-emerald-50 object-cover object-center ring-[0.5px] ring-black ring-opacity-15"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
