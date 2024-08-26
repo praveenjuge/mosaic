62c4c0f64db9ac32639e19c32bb664375e08e160
Naveen <naveendurai19@gmail.com>
2024-08-27 01:10:12 +0530

    Showing up the error message in production

--- app/websites/[slug]/page.tsx
+++ app/websites/[slug]/page.tsx
@@ -71,11 +71,11 @@ export default async function Page({ params }: { params: { slug: string } }) {
     }
   } catch (error) {
     console.error("Error fetching website data:", error);
-    return <div>Error loading website data...</div>;
+    return <div>{error?.message}</div>;
   }
 
   if (!websiteData) {
-    return <div>Loading website data...</div>;
+    return <div></div>;
   }
 
 
