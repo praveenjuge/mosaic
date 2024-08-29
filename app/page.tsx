adf98a7a7bec816480bce8c29f6e4eef350be095
Naveen <naveendurai19@gmail.com>
2024-08-29 20:47:10 +0530

    Moved latest screenshots to server side page

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,6 @@ import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { Metadata } from "next";
 import { Suspense } from "react";
 import WebsitesTable from "./websites/WebsitesTable";
-import FetchWebsitePagesData, { WebsitePageData } from "@/components/server/fetch-website-pages-data";
-import { auth } from "@clerk/nextjs/server";
 
 export const metadata: Metadata = {
   title: website_subtitle,
@@ -18,26 +16,7 @@ export const metadata: Metadata = {
   openGraph: { images: [getOgImageUrl("")] },
 };
 
-export default async function Home() {
-
-  let websitePagesData: WebsitePageData[] = [];
-
-  try {
-    const { getToken } = auth();
-    const token = await getToken({ template: "supabase" });
-    if (token) {
-      try {
-        const response = await FetchWebsitePagesData({ page: 1, limit: 5 });
-        websitePagesData = response.data;
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    } else {
-      console.log("No token available");
-    }
-  } catch (error) {
-    console.error("Error:", error);
-  }
+export default function Home() {
 
   return (
     <>
@@ -61,7 +40,7 @@ export default async function Home() {
           <CardTitle>Latest Screenshots</CardTitle>
         </CardHeader>
         <Suspense fallback={<Skeleton className="h-24 w-full rounded-lg" />}>
-          <LatestScreenshots websitePagesData={websitePagesData} />
+          <LatestScreenshots />
         </Suspense>
       </SignedIn>
     </>
