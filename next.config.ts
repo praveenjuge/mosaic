6ed3b0f267bfe3f772afdd135281bec068cfacf9
Praveen Juge <hello@praveenjuge.com>
2024-08-29 13:17:56 +0530

    Remove sentry for edge to make sure middlware is fast
    Signed-off-by: Praveen Juge <hello@praveenjuge.com>

--- next.config.ts
+++ next.config.ts
@@ -81,8 +81,6 @@ export default withSentryConfig(bundleAnalyzer(nextConfig), {
 
   org: "mosaicimg",
   project: "mosaic-frontend",
-
-  // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
 
   // For all available options, see:
@@ -90,16 +88,8 @@ export default withSentryConfig(bundleAnalyzer(nextConfig), {
 
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
-
-  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  // tunnelRoute: "/monitoring",
-
   // Hides source maps from generated client bundles
   hideSourceMaps: true,
-
   // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
   automaticVercelMonitors: false,
