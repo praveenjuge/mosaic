1b3f51f29b9c5d2f0ccd272d52ee9419a454b183
Naveen <naveendurai19@gmail.com>
2024-09-13 01:26:24 +0530

    refresh website button is added

---
+++ app/websites/refresh-site-button.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { backend_url } from "@/lib/constants";
+import { LoadingSpinner } from "@/components/spinner";
+import { Button } from "@/components/ui/button";
+
+import React from "react";
+import { useFormStatus } from "react-dom";
+import { toast } from "sonner";
+
+interface RefreshSiteButtonProps {
+  token: string;
+  websiteId: string;
+  text?: string;
+  variant?: "default" | "destructive" | "outline" | "secondary" | "ghost";
+}
+
+export function RefreshSiteButton({
+  token,
+  websiteId,
+  text = "Refresh Site",
+  variant = "default",
+}: RefreshSiteButtonProps) {
+
+  const { pending } = useFormStatus();
+
+  const [loading, setLoading] = React.useState(false);
+
+  const handleRefresh = async () => {
+    setLoading(true);
+
+    if (token) {
+      const response = await fetch(`${backend_url}websites/${websiteId}/refresh`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      try {
+        const data = await response.json();
+        if (response.ok) {
+          toast.success(data.message);
+        } else {
+          toast.error(data.message);
+        }
+      }
+      catch (error) {
+        console.error(error);
+      }
+    }
+    setLoading(false);
+    console.log("Refreshed!");
+  };
+
+  return (
+    <Button
+      type="button"
+      className=""
+      variant={variant}
+      disabled={pending || loading}
+      aria-disabled={pending || loading}
+      onClick={handleRefresh}
+    >
+      {loading ? <LoadingSpinner /> : text}
+    </Button>
+  );
+}
+
