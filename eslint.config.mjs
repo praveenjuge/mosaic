7514a042c3291e1abd01d4a50d8f2918d6b400e6
Praveen Juge <hello@praveenjuge.com>
2025-06-11 18:04:00 +0530

    refactor: add ESLint configuration and unused imports plugin for improved code quality

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
+import unusedImports from "eslint-plugin-unused-imports";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
@@ -25,10 +26,27 @@ export default [
   {
     plugins: {
       "@typescript-eslint": typescriptEslint,
+      "unused-imports": unusedImports,
     },
 
     languageOptions: {
       parser: tsParser,
+    },
+
+    rules: {
+      // Remove unused imports automatically
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+      // Disable the base rule as it can report incorrect errors
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
 ];
